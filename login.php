99bfea12f5dce9e0cc72523e5f053c7925680840
wilbertwhite <wilbertcwhite@gmail.com>
2022-07-09 18:41:18 -0400

    added login homepage logout and style.css

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css" />
+    <title>Log In</title>
+</head>
+
+<body>
+    <div class="main-menu">
+        <h1>Enter your name:</h1>
+        <form method="post" action="index.php">
+            <input type="text" name="player_name" placeholder="Name" />
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
+
+</body>
+
+</html>
